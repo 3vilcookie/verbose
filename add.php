6335f577f7c531977a47a21c00b79abdb242c326
Raphael Pour <info@evilcookie.de>
2019-01-12 13:45:30 +0000

    add.php edited online with Bitbucket

--- add.php
+++ add.php
@@ -1,13 +1,20 @@
 <?php
-require_once 'Vocabulary.php';
-$de = filter_input(INPUT_POST, 'de', FILTER_SANITIZE_SPECIAL_CHARS);
-$en = filter_input(INPUT_POST, 'en', FILTER_SANITIZE_SPECIAL_CHARS);
+    /*
+     * @file:    add.php
+     * @brief:   Retreives a new word from the formular of the mainpage,
+     *           filters and hands it over to the Vocabulary-API
+     * @author:  Raphael Pour <info@raphaelpour.de>
+     * @date:    01-2019
+     */
 
-if($de && $en)
-{   
-    $voc = new Vocabulary();
-    $voc->addWord($de, $en);
+    require_once 'Vocabulary.php';
+    $de = filter_input(INPUT_POST, 'de', FILTER_SANITIZE_SPECIAL_CHARS);
+    $en = filter_input(INPUT_POST, 'en', FILTER_SANITIZE_SPECIAL_CHARS);
 
-}
-include 'index.php'; 
+    if($de && $en)
+    {   
+        $voc = new Vocabulary();
+        $voc->addWord($de, $en);
+    }
+    include 'index.php'; 
 ?>
