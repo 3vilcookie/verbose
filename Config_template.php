f8f763c592e6c1a0184130849352734190c8d490
Raphael Pour <info@evilcookie.de>
2019-01-12 13:33:29 +0000

    Config_template.php edited online with Bitbucket

--- Config_template.php
+++ Config_template.php
@@ -1,5 +1,13 @@
 <?php
-$CONFIG = array();
-$CONFIG['db_password'] =  "<YOUR_PASSWORD>";
-$CONFIG['db_user'] = "<YOUR_USER>";
+    /*
+     * @file:    Config_template.php
+     * @brief:   Global Configuration of the Vocabulary-App including
+	 *           credentials for the db access
+     * @author:  Raphael Pour <info@raphaelpour.de>
+     * @date:    01-2019
+     */
+	
+	$CONFIG = array();
+	$CONFIG['db_password'] =  "<YOUR_PASSWORD>";
+	$CONFIG['db_user'] = "<YOUR_USER>";
 ?>
